293f0191b2dd5c953859b1c85c1ff12fd59e22d8
Lucas VALLEGEAS <954-luvallegea@users.noreply.gitlab.isima.fr>
2024-08-07 19:45:53 +0000

    Add /legal, /cgv and /privacy pages

---
+++ src/routes/cgv/+page.svelte
@@ -0,0 +1,38 @@
+<svelte:head>
+	<title>Condition Générale de vente</title>
+	<meta name="description" content="Conditions générales de vente" />
+</svelte:head>
+
+<article class="space-y-10 px-4 max-w-[1024px] lg:m-auto md:px-8">
+
+  <div class="text-center space-y-4">
+    <h1 class="font-bold text-3xl">Conditions générales de vente</h1>
+    <p class="italic">Mis à jour le: XX/XX/XXXX</p>
+  </div>
+  <!-- Les articles suivants définissent les conditions générales de vente des produits et services proposés par l'association Drezzing. En passant commande, vous acceptez ces conditions générales de vente. -->
+  <div class="text-justify space-y-8 ">
+    <section class="space-y-2">
+      <h2 class="font-extrabold">Article 1 - Objet</h2>
+      <h3 class="font-semibold">Sous titre 1</h3>
+      <p>Les présentes conditions régissent les ventes par l'association Drezzing de produits et services proposés sur le site internet www.drezzing.fr.</p>
+    </section>
+
+    <section class="space-y-2">
+      <h2 class="font-extrabold">Article 2 - Prix</h2>
+      <h3 class="font-semibold">Sous titre 2</h3>
+      <p>Les prix de nos produits sont indiqués en euros toutes taxes comprises (TVA et autres taxes applicables au jour de la commande), sauf indication contraire et hors frais de traitement et d'expédition.</p>   
+    </section>
+
+    <section class="space-y-2">
+      <h2 class="font-extrabold">Article 3 - Commandes</h2>
+      <h3 class="font-semibold">Sous titre 3</h3>
+      <p>Vous pouvez passer commande sur le site internet www.drezzing.fr.</p>
+    </section>
+
+    <section class="space-y-2">
+      <h2 class="font-extrabold">Article 4 - Validation de votre commande</h2>
+      <h3 class="font-semibold">Sous titre 4</h3>
+      <p>Toute commande figurant sur le site internet www.drezzing.fr suppose l'adhésion aux présentes Conditions Générales. Toute confirmation de commande entraîne votre adhésion pleine et entière aux présentes conditions générales de vente, sans exception ni réserve.</p>
+    </section>
+  </div>
+</article>
