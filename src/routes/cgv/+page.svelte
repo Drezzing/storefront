3fb33b72bbd075a169687f6a4dbb20d11471524f
happyfury006 <happyfury006@gmail.com>
2024-08-07 11:52:23 +0200

    mis en page des mentions legales, politiques de conf, cgv faite il reste plus qu'a les modifiés pour qu'ils soit conformes au différents documents qu'il faut écrire

---
+++ src/routes/cgv/+page.svelte
@@ -0,0 +1,96 @@
+<svelte:head>
+	<title>cgv</title>
+	<meta name="description" content="Conditions générales de vente" />
+</svelte:head>
+
+<div class="flex-initial w-auto relative">
+  <center>
+    <div class="flex-initial w-auto justify-center static  top-0  text-4xl font-black">
+    Conditions générales de vente
+    </div>
+    <div class="flex-initial w-64 justify-center static mt-5 italic">
+    Mis a jour le: XX/XX/XXXX
+    </div>
+  </center>
+  <!-- Les articles suivants définissent les conditions générales de vente des produits et services proposés par l'association Drezzing. En passant commande, vous acceptez ces conditions générales de vente. -->
+  <div class="mt-10 mb-20 justify-start">
+    <!-- la suite est a développer en fonction des conditions générales de ventes -->
+    <div class="flex-initial mt-5 ml-4 w-32 text-2xl font-bold ">
+      Titre 1
+    </div>
+    <div class="flex-initial mt-2 ml-6 w-32 text-lg italic font-medium">
+      Sous-titre
+    </div>
+    <div class="flex-initial text-justify min-w-min text-base m-4">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc tincidunt tincidunt. Nullam
+        quis
+        mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec
+        nunc
+        tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt
+      </p>
+    </div>
+    <div class="flex-initial mt-5 ml-4 w-32 text-2xl font-bold ">
+      Titre 1
+    </div>
+    <div class="flex-initial mt-2 ml-6 w-32 text-lg italic font-medium">
+      Sous-titre
+    </div>
+    <div class="flex-initial text-justify min-w-min text-base m-4">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc tincidunt tincidunt. Nullam
+        quis
+        mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec
+        nunc
+        tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt
+      </p>
+    </div>
+    <div class="flex-initial mt-5 ml-4 w-32 text-2xl font-bold ">
+      Titre 1
+    </div>
+    <div class="flex-initial mt-2 ml-6 w-32 text-lg italic font-medium">
+      Sous-titre
+    </div>
+    <div class="flex-initial text-justify min-w-min text-base m-4">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc tincidunt tincidunt. Nullam
+        quis
+        mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec
+        nunc
+        tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt
+      </p>
+    </div>
+    <div class="flex-initial mt-5 ml-4 w-32 text-2xl font-bold ">
+      Titre 1
+    </div>
+    <div class="flex-initial mt-2 ml-6 w-32 text-lg italic font-medium">
+      Sous-titre
+    </div>
+    <div class="flex-initial text-justify min-w-min text-base m-4">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc tincidunt tincidunt. Nullam
+        quis
+        mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec
+        nunc
+        tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt
+      </p>
+    </div>
+    <div class="flex-initial mt-5 ml-4 w-32 text-2xl font-bold ">
+      Titre 1
+    </div>
+    <div class="flex-initial mt-2 ml-6 w-32 text-lg italic font-medium">
+      Sous-titre
+    </div>
+    <div class="flex-initial text-justify min-w-min text-base m-4">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc tincidunt tincidunt. Nullam
+        quis
+        mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec
+        nunc
+        tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt tincidunt. Nullam quis mauris nec nunc tincidunt
+      </p>
+    </div>
+  </div>
+  
+  
+</div>
---
+++ src/routes/legal/+page.svelte
@@ -0,0 +1,84 @@
+<svelte:head>
+	<title>legal</title>
+	<meta name="description" content="Mentions légales" />
+</svelte:head>
+
+<div class="flex-initial w-auto relative">
+  <center>
+    <div class="flex-initial w-auto justify-center static  top-0  text-4xl font-black">
+    Mentions légales
+    </div>
+    <div class="flex-initial w-64 justify-center static mt-5 italic">
+    Mis a jour le: XX/XX/XXXX
+    </div>
+  </center>
+  
+  <div class="mt-10 mb-20 justify-start">
+    <div class="justify-start ml-4 text-2xl font-bold">
+      Identification de l'éditeur et de l'hébergeur du site
+    </div>  
+    <div class="flex-initial mt-5 ml-4 w-32 text-xl font-bold ">
+      Editeur :
+    </div>
+    <div class="flex-initial text-justify min-w-min text-base m-4">
+      <p>
+        Association Drezzing 
+      </p>
+      
+      <p>
+        Adresse :
+        <a href="https://www.google.com/maps/search/?api=1&query=1+rue+de+la+Chebarde+63100+Aubière+France" target="_blank">
+          1 rue de la Chebarde
+          63100 Aubière
+          France
+        </a>
+      </p>
+      <p>
+        Tel: <a href="tel:0473000000">04 73 00 00 00</a>
+      </p>
+      <p>
+        Email: <a href="mailto:contact@drezzing.fr">contact@drezzing.fr</a>
+      </p>
+      <p>
+        Numéro SIRET : <a href="https://www.sirene.fr/sirene/public/recherche?recherche.siren_siret=12345678901234" target="_blank">12345678901234</a>
+      </p>
+      <p>
+        Directeur de la publication : Nathan Harraud
+      </p>
+    </div>
+    <div class="flex-initial mt-5 ml-4 w-32 text-xl font-bold">
+      Hébergeur :
+    </div>
+    <div class="flex-initial text-justify min-w-min text-base m-4">
+      <a href="https://www.ovh.com" target="_blank">OVH</a>
+      <p>
+      SAS au capital de 10 069 020 €
+      </p>
+      <p>
+      RCS Lille Métropole 424 761 419 00045
+      </p>
+      <p>
+      Code APE 2620Z
+      </p>
+      <p>
+      N° TVA : FR 22 424 761 419
+      </p>
+      <p>
+      Siège social : <a href="https://www.google.com/maps/search/?api=1&query=2+rue+Kellermann+-+59100+Roubaix+-+France" target="_blank">2 rue Kellermann - 59100 Roubaix - France</a>
+      </p>
+    
+    </div>
+    
+    <div class="flex-initial mt-5 ml-4 w-32 text-xl font-bold">
+      Propriété intellectuelle
+    </div>
+    <div class="flex-initial text-justify min-w-min text-base m-4">
+      <p>
+        L'ensemble des éléments du site www.drezzing.fr, notamment les textes, présentations, illustrations, photographies, arborescences et mises en forme sont, sauf documents publics et précisions complémentaires, la propriété intellectuelle exclusive de l'association Drezzing ou de ses partenaires.
+        Toute reproduction, représentation, publication, transmission, ou plus généralement toute exploitation non autorisée du site et/ou de ses éléments engage votre responsabilité et est susceptible d'entraîner des poursuites judiciaires.
+        L'ensemble du contenu de l'application Drezzing, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de l'association Drezzing à l'exception des marques, logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs.
+        Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l'accord exprès par écrit de Drezzing. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur.
+      </p>
+    </div>
+  </div>
+</div>
