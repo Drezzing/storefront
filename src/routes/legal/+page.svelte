a93bc17141ac297636a7ab77e2ebb1c682eaa011
Lucas VALLEGEAS <954-luvallegea@users.noreply.gitlab.isima.fr>
2024-08-08 19:57:07 +0000

    Add homepage template

--- src/routes/legal/+page.svelte
+++ src/routes/legal/+page.svelte
@@ -1,51 +1,74 @@
 <svelte:head>
-	<title>Mentions légales</title>
-	<meta name="description" content="Mentions légales" />
+    <title>Mentions légales</title>
+    <meta name="description" content="Mentions légales" />
 </svelte:head>
 
-<article class="px-4 max-w-[1024px] lg:m-auto md:px-8">
+<article class="max-w-[1024px] px-4 md:px-8 lg:m-auto">
+    <div class="mb-10 space-y-4 text-center">
+        <h1 class="text-3xl font-bold">Mentions légales</h1>
+        <p class="italic">Mis à jour le: XX/XX/XXXX</p>
+    </div>
 
-  <div class="text-center space-y-4 mb-10">
-    <h1 class="font-bold text-3xl">Mentions légales</h1>
-    <p class="italic">Mis à jour le: XX/XX/XXXX</p>
-  </div>
-  
-  <h2 class="font-bold mb-4 text-xl">Identification de l'éditeur et de l'hébergeur du site</h2>
-  
-  <div class="space-y-8 text-justify mt-0">  
-    
-    <section class="space-y-4">
-      <h3 class="font-bold">Editeur :</h3>
-      <ul class="space-y-1">
-        <li>Association Drezzing</li>
-        <li>Adresse : <a href="https://www.google.com/maps/search/?api=1&query=1+rue+de+la+Chebarde+63100+Aubière+France" target="_blank">1 rue de la Chebarde 63100 Aubière France</a></li>
-        <li>Tel: <a href="tel:0473000000">04 73 00 00 00</a></li>
-        <li>Email: <a href="contact@drezzing.fr">contact@drezzing.fr</a></li>
-        <li>Numéro SIRET : <a href="https://www.sirene.fr/sirene/public/recherche?recherche.siren_siret=12345678901234" target="_blank">12345678901234</a></li>
-        <li>Directeur de la publication : Nathan Harraud</li>
-      </ul>
-    </section>
-    
-    <section class="space-y-4">
-      <h3 class="font-bold">Hébergeur :</h3>
-      <ul class="space-y-1">
-        <li><a href="https://www.ovh.com" target="_blank">OVH</a></li>
-        <li>SAS au capital de 10 069 020 €</li>
-        <li>RCS Lille Métropole 424 761 419 00045</li>
-        <li>Code APE 2620Z</li>
-        <li>N° TVA : FR 22 424 761 419</li>
-        <li>Siège social : <a href="https://www.google.com/maps/search/?api=1&query=2+rue+Kellermann+-+59100+Roubaix+-+France" target="_blank">2 rue Kellermann - 59100 Roubaix - France</a></li>
-      </ul>
-    </section>
-    
-    <section class="space-y-4">
-      <h3 class="font-bold">Propriété intellectuelle</h3>
-      <p>
-        L'ensemble des éléments du site www.drezzing.fr, notamment les textes, présentations, illustrations, photographies, arborescences et mises en forme sont, sauf documents publics et précisions complémentaires, la propriété intellectuelle exclusive de l'association Drezzing ou de ses partenaires.
-        Toute reproduction, représentation, publication, transmission, ou plus généralement toute exploitation non autorisée du site et/ou de ses éléments engage votre responsabilité et est susceptible d'entraîner des poursuites judiciaires.
-        L'ensemble du contenu de l'application Drezzing, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de l'association Drezzing à l'exception des marques, logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs.
-        Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l'accord exprès par écrit de Drezzing. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur.
-      </p>
-    </section>
+    <h2 class="mb-4 text-xl font-bold">Identification de l'éditeur et de l'hébergeur du site</h2>
 
+    <div class="mt-0 space-y-8 text-justify">
+        <section class="space-y-4">
+            <h3 class="font-bold">Editeur :</h3>
+            <ul class="space-y-1">
+                <li>Association Drezzing</li>
+                <li>
+                    Adresse : <a
+                        href="https://www.google.com/maps/search/?api=1&query=1+rue+de+la+Chebarde+63100+Aubière+France"
+                        target="_blank">1 rue de la Chebarde 63100 Aubière France</a
+                    >
+                </li>
+                <li>Tel: <a href="tel:0473000000">04 73 00 00 00</a></li>
+                <li>Email: <a href="contact@drezzing.fr">contact@drezzing.fr</a></li>
+                <li>
+                    Numéro SIRET : <a
+                        href="https://www.sirene.fr/sirene/public/recherche?recherche.siren_siret=12345678901234"
+                        target="_blank">12345678901234</a
+                    >
+                </li>
+                <li>Directeur de la publication : Nathan Harraud</li>
+            </ul>
+        </section>
+
+        <section class="space-y-4">
+            <h3 class="font-bold">Hébergeur :</h3>
+            <ul class="space-y-1">
+                <li><a href="https://www.ovh.com" target="_blank">OVH</a></li>
+                <li>SAS au capital de 10 069 020 €</li>
+                <li>RCS Lille Métropole 424 761 419 00045</li>
+                <li>Code APE 2620Z</li>
+                <li>N° TVA : FR 22 424 761 419</li>
+                <li>
+                    Siège social : <a
+                        href="https://www.google.com/maps/search/?api=1&query=2+rue+Kellermann+-+59100+Roubaix+-+France"
+                        target="_blank">2 rue Kellermann - 59100 Roubaix - France</a
+                    >
+                </li>
+            </ul>
+        </section>
+
+        <section class="space-y-4">
+            <h3 class="font-bold">Propriété intellectuelle</h3>
+            <p>
+                L'ensemble des éléments du site www.drezzing.fr, notamment les textes, présentations, illustrations,
+                photographies, arborescences et mises en forme sont, sauf documents publics et précisions
+                complémentaires, la propriété intellectuelle exclusive de l'association Drezzing ou de ses partenaires.
+                Toute reproduction, représentation, publication, transmission, ou plus généralement toute exploitation
+                non autorisée du site et/ou de ses éléments engage votre responsabilité et est susceptible d'entraîner
+                des poursuites judiciaires. L'ensemble du contenu de l'application Drezzing, incluant, de façon non
+                limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que
+                leur mise en forme sont la propriété exclusive de l'association Drezzing à l'exception des marques,
+                logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs. Toute reproduction,
+                distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents
+                éléments est strictement interdite sans l'accord exprès par écrit de Drezzing. Cette représentation ou
+                reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles
+                L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction
+                constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur.
+            </p>
+        </section>
+    </div>
 </article>
