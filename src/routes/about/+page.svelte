c8b25b239d4ea370cb363d1d72084c094b003959
Julen SOMET-GARCIA <julen.somet-garcia@etu.uca.fr>
2024-08-30 20:33:15 +0000

    Add about page (!10)
    See merge request drezzing/drezzing-frontend!10

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,33 @@
+<svelte:head>
+    <title>À propos</title>
+    <meta name="description" content="À propos" />
+</svelte:head>
+
+<article class="max-w-[1024px] px-4 md:px-8 lg:m-auto">
+    <div class="mb-8 mt-8 text-center md:mb-12 md:mt-12 lg:mb-16 lg:mt-16">
+        <h1 class="text-3xl font-bold">À propos</h1>
+    </div>
+    <div class="mt-0 space-y-8 text-justify md:space-y-12 lg:space-y-16">
+        <div>
+            <h2 class="mb-4 text-xl font-bold">Qui sommes-nous ?</h2>
+            <p>
+                Nous somme un groupe d'étudiants de l'ISIMA qui souhaite aider les étudiants de l'ISIMA à trouver des
+                produits textiles ou goodies à l'image de leur école et de ses associations.
+            </p>
+        </div>
+
+        <div>
+            <h2 class="mb-4 text-xl font-bold">Notre objetif</h2>
+            <p>
+                Notre objectif est de permettre aux étudiants mais aussi aux différents clubs et associations de l'ISIMA
+                de trouver des produits au meilleur prix. Nous travaillons avec différents fournisseurs pour trouver les
+                meilleurs offres et agissons en tant qu'intermédiaire pour l'école, les associations et les clubs.
+            </p>
+        </div>
+
+        <div>
+            <h2 class="mb-4 text-xl font-bold">Notre équipe</h2>
+            <p>Notre équipe est composée de 9 étudiants de l'ISIMA.</p>
+        </div>
+    </div>
+</article>
