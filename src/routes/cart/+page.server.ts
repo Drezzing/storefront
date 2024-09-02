dc2554d12efe4b67dac936e4a7c59a61ba914ce8
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2024-09-02 14:41:06 +0200

    fix(cart): proper page rendering

--- src/routes/cart/+page.server.ts
+++ src/routes/cart/+page.server.ts
@@ -2,7 +2,9 @@ import type { CartType } from "$lib/cart/cart";
 import { handleError } from "$lib/error.js";
 import { checkCartExists, medusa } from "$lib/medusa/medusa";
 
-export const csr = false;
+export const prerender = false;
+export const ssr = true;
+export const csr = true;
 
 export const load = async ({ cookies }) => {
     const cartId = cookies.get("panier");
