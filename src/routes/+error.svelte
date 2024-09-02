0092922cc8985923a1efcd0d3bbb9e64feaa3e74
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2024-09-02 14:41:27 +0200

    feat(errors): add error component

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<div class="flex h-[75vh] flex-col items-center justify-center gap-4">
+    <h1 class="text-3xl font-bold">Une erreur est survenue</h1>
+    <p>Code erreur : {$page.status} - {$page.error?.message}</p>
+</div>
