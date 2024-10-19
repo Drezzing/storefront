bfb33cc6616171142bec204baa338eb4b47e7327
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2024-10-19 11:50:15 +0200

    feat: add validation to /cart/complete redirect status, fixes #14

--- src/routes/cart/complete/+page.server.ts
+++ src/routes/cart/complete/+page.server.ts
@@ -1,11 +1,19 @@
 import { handleError } from "$lib/error.js";
+import { z } from "zod";
+
+const searchParam = z.union([z.literal("succeeded"), z.literal("pending"), z.literal("failed")]);
 
 export const csr = true;
 
 export const load = async ({ url, cookies }) => {
-    const redirectStatus = url.searchParams.get("redirect_status") as "succeeded" | "pending" | "failed" | null;
+    const redirectStatus = url.searchParams.get("redirect_status");
     if (!redirectStatus) {
         return handleError(400, "CART_COMPLETE_LOAD.MISSING_PARAM");
+    }
+
+    const redirectStatusValid = searchParam.safeParse(redirectStatus);
+    if (!redirectStatusValid.success) {
+        return handleError(400, "CART_COMPLETE_LOAD.INVALID_PARAM");
     }
 
     if (redirectStatus === "succeeded") {
