5d9dd275b2b19fe5adde3bdeb42772b300ebf351
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2024-11-17 17:00:54 +0100

    refactor: update cgv

--- src/routes/cgv/+page.svelte
+++ src/routes/cgv/+page.svelte
@@ -6,47 +6,32 @@
 <article class="max-w-[1024px] space-y-10 px-4 md:px-8 lg:m-auto">
     <div class="space-y-4 text-center">
         <h1 class="text-3xl font-bold">Conditions générales de vente</h1>
-        <p class="italic">Mis à jour le: 29/08/2024</p>
+        <p class="italic">Mis à jour le: 17/11/2024</p>
     </div>
     <div class="space-y-8 text-justify">
         <section class="space-y-2">
             <h2 class="font-extrabold">Article 1 : Définitions</h2>
-            <ul class="space-y-2">
-                <li>
-                    <strong>Association</strong>:
-                    <ul>
-                        <li>DREZZING, Association loi 1901</li>
-                        <li>SIREN : 987 817 061</li>
-                        <li>RNA : W632014771</li>
-                        <li>Code APE : 9412Z</li>
-                        <li>Siège social : Isima, 1 rue de la Chebarde - 63170 Aubière</li>
-                    </ul>
-                </li>
-                <li>
-                    <strong>Client</strong>: Toute personne physique ou morale procédant à un achat sur la boutique en
-                    ligne de l’association ou au foyer de l’école.
-                </li>
-                <li><strong>Adhérent</strong>: Membre adhérent de l’association.</li>
-                <li>
-                    <strong>Utilisateur</strong>: Toute personne naviguant sur le site de la boutique en ligne de
-                    l’association.
-                </li>
-            </ul>
+            <p>
+                L’association DreZZing, association régie par la loi du 1er juillet 1901, domiciliée au 1 rue de la
+                Chebarde à Aubière (63170), enregistrée au répertoire SIRENE sous le numéro 987 817 061 sera désignée
+                ci-après par les termes « vendeur » ou « association ». La personne physique ou morale qui passe
+                commande auprès de l’association sera désignée ci-après par les termes « client » ou « acheteur ».
+            </p>
         </section>
 
         <section class="space-y-2">
             <h2 class="font-extrabold">Article 2 : Objet</h2>
             <p>
                 Les présentes conditions générales de vente (CGV) régissent les ventes de produits effectuées par
-                l’association DREZZING via son site internet et au foyer de l’école.
+                l’association DREZZING via son site web (drezzing.fr) et au foyer de l’école.
             </p>
         </section>
 
         <section class="space-y-2">
             <h2 class="font-extrabold">Article 3 : Produits</h2>
             <p>
-                Les produits proposés à la vente sont ceux décrits sur le site de l’association. L’association s’efforce
-                de fournir des descriptions aussi précises que possible.
+                Les produits proposés à la vente sont ceux décrits et présentés sur le site web de l’association.
+                L’association s’efforce de fournir des descriptions aussi précises que possible.
             </p>
         </section>
 
@@ -59,72 +44,106 @@
         </section>
 
         <section class="space-y-2">
-            <h2 class="font-extrabold">Article 5 : Commande</h2>
+            <h2 class="font-extrabold">Article 5 : Offres promotionnelles et réductions</h2>
             <p>
-                Le Client passe commande sur le site internet de l’association ou achète directement au foyer de
+                L’association peut proposer des réductions et offres promotionnelles aux adhérents. Les réductions et
+                offres promotionnelles proposées ainsi que leurs conditions d'éligibilité sont décrites dans les
+                conditions particulières de vente, lorsqu'il y a lieu.
+            </p>
+        </section>
+
+        <section class="space-y-2">
+            <h2 class="font-extrabold">Article 6 : Commande</h2>
+            <p>
+                Le client passe commande sur le site internet de l’association ou achète directement au foyer de
                 l’école. Toute commande vaut acceptation des prix et descriptions des produits disponibles à la vente.
             </p>
         </section>
 
         <section class="space-y-2">
-            <h2 class="font-extrabold">Article 6 : Modalités de paiement</h2>
+            <h2 class="font-extrabold">Article 7 : Modalités de paiement</h2>
             <p>
-                Le paiement peut être effectué en ligne par carte ou virement bancaire via Paypal ou Stripe, ou sur
-                place au foyer de l’école par carte bancaire ou en espèces.
+                Le paiement peut être effectué en ligne par carte bancaire via Paypal ou Stripe, ou sur place au foyer
+                de l’école par carte bancaire ou en espèces. Il convient que l’acheteur doit faire l’appoint puisque le
+                vendeur ne rend pas la monnaie.
             </p>
         </section>
 
         <section class="space-y-2">
-            <h2 class="font-extrabold">Article 7 : Droit de rétractation</h2>
+            <h2 class="font-extrabold">Article 8 : Délais et retards de paiement</h2>
             <p>
-                Le Client dispose d’un délai de rétractation de 14 jours à compter de la réception des articles. Les
-                articles retournés doivent être en parfait état et dans leur emballage d’origine.
+                Le vendeur se réserve le droit d’annuler la vente si le client ne s’est pas acquitté des sommes dues
+                après un rappel infructueux du vendeur.
+            </p>
+        </section>
+
+        <section class="space-y-2">
+            <h2 class="font-extrabold">Article 9 : Fraude et recouvrement</h2>
+            <p>
+                Si le client bénéficie d'une réduction ou d’une offre promotionnelle à laquelle il n'est pas éligible
+                l'association peut le lui faire remarquer en le notifiant par courrier électronique à l'adresse fournie
+                lors de la commande.
+            </p>
+            <p>
+                Dans une telle situation, le client dispose d’une période de recouvrement de quatorze (14) à compter de
+                la notification pour renoncer à sa commande, contre une indemnité forfaitaire de dix (10) euros, ou
+                payer la différence de prix entre le tarif normal et le tarif réduit.
+            </p>
+            <p>
+                Si la commande fait partie d’une opération commerciale, la période de recouvrement ne peut échoir après
+                la fin de l’opération commerciale.
+            </p>
+        </section>
+
+        <section class="space-y-2">
+            <h2 class="font-extrabold">Article 10 : Droit de rétractation</h2>
+            <p>
+                Le client dispose d’un délai de rétractation de 10 jours ouvrés à compter de la validation de la
+                commande lui permettant de renoncer à cette dernière.
+            </p>
+        </section>
+
+        <section class="space-y-2">
+            <h2 class="font-extrabold">Article 11 : Livraison</h2>
+            <p>
+                Le client ne peut pas retirer sa commande s’il ne s’est pas acquitté du montant total dû à
+                l’association. Les commandes sont à retirer au foyer de l’école, sauf contre-indications spécifiées par
+                les conditions particulières de vente.
             </p>
         </section>
 
         <section class="space-y-2" id="retours-echanges">
-            <h2 class="font-extrabold">Article 8 : Retours et échanges</h2>
+            <h2 class="font-extrabold">Article 12 : Retours et échanges</h2>
             <p>
-                L’association peut reprendre mais ne rembourse pas les articles. Elle se réserve le droit de proposer un
-                avoir du montant total des articles retournés, valable 1 an sur tous les tous les articles éligibles
-                dans la boutique en ligne de l’association ou au foyer de l’école.
+                Le client dispose de 10 jours ouvrés dès la réception de sa commande pour éventuellement retourner à
+                l’association des articles qui ne lui conviendraient plus.
+            </p>
+            <p>
+                Les articles retournés doivent être en parfait état et dans leur emballage d’origine. Le cas échéant,
+                l’association se réserve le droit de refuser les articles retournés.
+            </p>
+            <p>L’association n’accepte pas les échanges.</p>
+        </section>
+
+        <section class="space-y-2">
+            <h2 class="font-extrabold">Article 13 : Remboursements</h2>
+            <p>
+                Les produits vendus par l’association ne sont pas remboursables. En revanche, l’association peut
+                proposer des avoirs sur les produits vendus, à faire valoir une (1) fois seulement pendant une durée
+                d’un (1) an à compter de la date d’obtention.
             </p>
         </section>
 
         <section class="space-y-2">
-            <h2 class="font-extrabold">Article 9 : Offres promotionnelles et réductions</h2>
-            <p>L’association propose des réductions et offres promotionnelles aux adhérents.</p>
-            <p>
-                Les réductions et offres promotionnelles ainsi que les conditions d'éligibilité sont décrites dans les
-                conditions particulières de vente, lorsqu'il y a lieu.
-            </p>
-            <p>
-                Si le client bénéficie d'une réduction ou offre promotionnelle à laquelle il n'est pas éligible
-                l'association peut le lui faire remarquer en le notifiant pas courrier électronique à l'adresse fournie
-                lors de la commande. Dès lors, le client dispose de 14 jours pour payer la différence de prix entre le
-                tarif normal et le tarif réduit ou alors renoncer à sa commande contre remboursement du montant
-                déboursé.
-            </p>
-        </section>
-
-        <section class="space-y-2">
-            <h2 class="font-extrabold">Article 10 : Livraison</h2>
-            <p>
-                Les produits sont à retirer au foyer de l’école, sauf contre-indications spécifiées dans les conditions
-                particulières de vente.
-            </p>
-        </section>
-
-        <section class="space-y-2">
-            <h2 class="font-extrabold">Article 11 : Responsabilité</h2>
+            <h2 class="font-extrabold">Article 14 : Responsabilité</h2>
             <p>
                 L’association ne saurait être tenue pour responsable des dommages résultant d’une mauvaise utilisation
-                des produits achetés.
+                des produits achetés
             </p>
         </section>
 
         <section class="space-y-2">
-            <h2 class="font-extrabold">Article 12 : Litiges</h2>
+            <h2 class="font-extrabold">Article 15 : Litiges</h2>
             <p>
                 Les présentes CGV sont soumises à la loi française. En cas de litige, les parties s’efforceront de
                 trouver une solution amiable. À défaut, le tribunal compétent sera celui du siège social de
