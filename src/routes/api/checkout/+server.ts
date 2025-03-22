ec4132a13a5721bfc55b3bd9f48a25b53e076b58
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2025-03-22 21:34:05 +0100

    refactor(checkout): clean-up dead code

--- src/routes/api/checkout/+server.ts
+++ src/routes/api/checkout/+server.ts
@@ -10,6 +10,10 @@ import { confirmationTokenData } from "$lib/checkout/formSchema.js";
 import { PUBLIC_BASE_URL } from "$env/static/public";
 import { PaymentNotification } from "$lib/checkout/notification.js";
 
+/**
+ * POST /api/checkout
+ * Submit the confirmation token to Stripe to confirm the payment intent.
+ */
 export const POST = async ({ cookies, request }) => {
     const cartInfo = await checkCartExists(cookies.get("panier"));
     if (cartInfo.err) {
@@ -45,7 +49,6 @@ export const POST = async ({ cookies, request }) => {
     const returnUrl = `/cart/complete?subscriber_key=${subcriberKey}`;
 
     const stripeConfirm = await stripe.paymentIntents.confirm(paymentId, {
-        // receipt_email: cartInfo.cart.customer.email,
         return_url: PUBLIC_BASE_URL + returnUrl,
         confirmation_token: confirmationTokenValid.data.confirmationToken,
     });
@@ -60,69 +63,6 @@ export const POST = async ({ cookies, request }) => {
         redirect: returnUrl,
     });
 };
-
-// export const POST = async ({ cookies, request }) => {
-//     const cartInfo = await checkCartExists(cookies.get("panier"));
-//     if (cartInfo.err) {
-//         return handleError(400, "CHECKOUT_POST.CART_INVALID", { cart_id: cookies.get("panier") });
-//     }
-
-//     if (cartInfo.cart.items.some((item) => isVariantSoldout(item.variant))) {
-//         return handleError(423, "CHECKOUT_POST.CART_ITEM_SOLDOUT");
-//     }
-
-//     if (cartInfo.cart.completed_at) {
-//         cookies.delete("panier", { path: "/" });
-//         return handleError(423, "CHECKOUT_POST.CART_ALREADY_COMPLETED", { cart_id: cartInfo.cart.id });
-//     }
-
-//     if (cartInfo.cart.payment_session) {
-//         return json({ client_secret: cartInfo.cart.payment_session.data.client_secret as string });
-//     }
-
-//     const reqJson = await request.json().catch(async () => {
-//         return handleError(400, "CHECKOUT_POST.INVALID_BODY", { body: await request.text() });
-//     });
-
-//     const userDataValid = CartUserData.safeParse(reqJson);
-//     if (!userDataValid.success) {
-//         return handleError(422, "CHECKOUT_POST.INVALID_DATA", { data: reqJson });
-//     }
-
-//     if (discountNotUsed(cartInfo.cart)) {
-//         await removeDiscounts(cartInfo.cart, "CHECKOUT_POST");
-//     }
-
-//     const { cart } = await medusa.carts
-//         .update(cartInfo.cart.id, {
-//             email: userDataValid.data.mail,
-//             billing_address: {
-//                 first_name: userDataValid.data.firstName,
-//                 last_name: userDataValid.data.lastName,
-//             },
-//         })
-//         .catch((err) => {
-//             return handleError(500, "CHECKOUT_POST.UPDATE_CART_FAILED", { error: err.response.data });
-//         });
-
-//     await medusa.carts.createPaymentSessions(cart.id).catch((err) => {
-//         return handleError(500, "CHECKOUT_POST.CREATE_PAYMENT_FAILED", { error: err.response.data });
-//     });
-
-//     const { cart: final } = await medusa.carts
-//         .setPaymentSession(cartInfo.cart.id, { provider_id: "stripe" })
-//         .catch((err) => {
-//             return handleError(500, "CHECKOUT_POST.SET_SESSION_FAILED", { error: err.response.data });
-//         });
-
-//     if (!final.payment_session) {
-//         return handleError(500, "CHECKOUT_POST.PAYEMENT_SESSION_MISSING", {
-//             error: `Payment session missing on cart ${final.id}`,
-//         });
-//     }
-
-//     return json({ client_secret: final.payment_session.data.client_secret as string });
-// };
 
 export const GET = async ({ cookies }) => {
     const cartInfo = await checkCartExists(cookies.get("panier"));
@@ -166,8 +106,6 @@ export const GET = async ({ cookies }) => {
             paypal: { preferred_locale: "fr-FR" },
         },
     });
-
-    // stripe.paymentIntents.update("sfq", { payment_method_options: { card: { request_three_d_secure: "any" } } });
 
     if (!cartFinal.cart.payment_session) {
         return handleError(500, "CHECKOUT_POST.PAYEMENT_SESSION_MISSING", {
