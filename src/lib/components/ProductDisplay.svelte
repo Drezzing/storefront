247b33c65647deaacb6f61a21edfb9a56e1e2c83
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2025-03-25 15:20:01 +0100

    fix(ProductDisplay): off by one and media query issue
    Closes #9

--- src/lib/components/ProductDisplay.svelte
+++ src/lib/components/ProductDisplay.svelte
@@ -14,7 +14,7 @@
         if (i > 4 && i <= 6) {
             visibility = "hidden sm:block";
         } else if (i > 6 && i <= 8) {
-            visibility = "hidden lg:block";
+            visibility = "hidden md:block";
         }
 
         return visibility;
@@ -23,7 +23,7 @@
 
 <div class="grid grid-cols-2 items-center justify-center gap-6 sm:grid-cols-3 md:grid-cols-4 lg:gap-8">
     {#each elements as element, i}
-        <a href="/{route}/{element.handle}" class="space-y-2 rounded-lg {limitNumber ? getVisibility(i) : ''}">
+        <a href="/{route}/{element.handle}" class="space-y-2 rounded-lg {limitNumber ? getVisibility(i + 1) : ''}">
             <img src={element.thumbnail} alt={element.title} class="aspect-square size-full object-contain" />
             <p class="pb-2 text-center">{element.title}</p>
         </a>
