6e01b7c05fa5dbac20dd332ed78ba830a2226f8d
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2025-04-07 11:40:42 +0200

    build: update chunking and preloading strategies

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [sveltekit()],
+    build: {
+        rollupOptions: {
+            output: {
+                experimentalMinChunkSize: 50000,
+            },
+        },
+    },
 });
