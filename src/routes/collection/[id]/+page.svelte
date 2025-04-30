cbe4cf1a7898be2a3588f85f0184ab504648f43f
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2025-04-30 11:46:44 +0200

    refactor(filter): move collection filter to its own component

--- src/routes/collection/[id]/+page.svelte
+++ src/routes/collection/[id]/+page.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
-    import { Filter } from "lucide-svelte";
-
     import ProductDisplay from "$lib/components/ProductDisplay.svelte";
     import { ProductFilter } from "$lib/components/ProductFilter/productFilter.svelte.js";
-    import SidePanel from "$lib/components/SidePanel.svelte";
-    import { Button } from "$lib/components/ui/button/index.js";
-    import { Checkbox } from "$lib/components/ui/checkbox";
-    import { Label } from "$lib/components/ui/label";
-    import { Separator } from "$lib/components/ui/separator/index.js";
-    import { Slider } from "$lib/components/ui/slider";
+    import ProductFilterPanel from "$lib/components/ProductFilter/ProductFilterPanel.svelte";
 
     let { data } = $props();
     const { title, thumbnail, products, description, cpv, guideTaille } = data;
@@ -55,53 +48,7 @@
 
         <section class="-mx-4 bg-[#EEEEEE] px-4 py-2">
             <div class="flex items-center justify-between">
-                <SidePanel>
-                    {#snippet trigger()}
-                        <div
-                            class="inline-flex items-center rounded-sm bg-white px-4 py-2 text-black shadow hover:bg-white/90"
-                        >
-                            <Filter strokeWidth={1} class="mr-2" />
-                            <p class="text-sm">Filtrer</p>
-                        </div>
-                    {/snippet}
-                    {#snippet title()}
-                        <h2 class="mx-4 mb-6 mt-3 text-2xl font-bold">Filtrer par</h2>
-                    {/snippet}
-
-                    <div class="mx-4 space-y-3">
-                        <h2 class="font-bold">Prix</h2>
-                        <span>{filter.selectedPrices[0]}€</span> à <span>{filter.selectedPrices[1]}€</span>
-                        <Slider class="mx-[8px] w-auto" bind:value={filter.selectedPrices} min={0} max={100} step={5} />
-                    </div>
-
-                    {#each filter.options as option (option[0])}
-                        {@const key = option[0]}
-                        {@const values = option[1]}
-                        <Separator class="mx-2 my-6 h-[2px] w-auto bg-[#EEEEEE] md:rounded-full" />
-
-                        <div class="mx-4 space-y-3">
-                            <h3 class="font-bold">{key}</h3>
-
-                            {#each values as value (value)}
-                                <div class="flex items-center gap-2">
-                                    <Checkbox
-                                        id={`${key}-${value}`}
-                                        class="data-[state=checked]:bg-d-darkgray"
-                                        checked={filter.selectedOptions.get(key)?.has(value) ?? false}
-                                        onCheckedChange={(v) => filter.handleCheckbox(key, value, v)}
-                                    />
-                                    <Label for={`${key}-${value}`}>{value}</Label>
-                                </div>
-                            {/each}
-                        </div>
-                    {/each}
-
-                    <div class="mb-8 flex flex-row items-center justify-center">
-                        <Button class="mx-4 mt-8 w-32" variant="drezzing" onclick={() => filter.resetOptions()}
-                            >Réinitialiser</Button
-                        >
-                    </div>
-                </SidePanel>
+                <ProductFilterPanel {filter} />
                 <p class="ml-4">{filter.selectedCount} produit{filter.selectedCount > 1 ? "s" : ""}</p>
             </div>
         </section>
