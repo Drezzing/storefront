d2bc4e6cc32686721b3a02b277f8047a42560b56
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2025-05-04 15:53:03 +0200

    make prettier happy

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,7 +7,7 @@
     import drezzing from "$lib/images/drezzing.png";
 </script>
 
-<header class="sticky top-0 bg-white z-50 mb-6 grid h-14 w-full grid-cols-3 items-center px-4 shadow lg:px-12">
+<header class="sticky top-0 z-50 mb-6 grid h-14 w-full grid-cols-3 items-center bg-white px-4 shadow lg:px-12">
     <div class="lg:hidden">
         <SidePanel>
             {#snippet trigger()}
