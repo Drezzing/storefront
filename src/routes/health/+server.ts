963158f0b18cfb9c7c111b1548eb04b1105f8939
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2025-05-11 22:05:22 +0200

    refactor(health): add partial option and improve robustness

--- src/routes/health/+server.ts
+++ src/routes/health/+server.ts
@@ -2,10 +2,22 @@ import env from "$lib/env/private";
 
 export const prerender = false;
 
-export const GET = async ({ fetch }) => {
-    const response = await fetch(env.get("MEDUSA_BACKEND_URL") + "/health");
+export const GET = async ({ fetch, url }) => {
+    // In some scenarios (like docker healthcheck) we only want to check if the sveltekit server is good
+    const partial = url.searchParams.get("partial");
+    if (partial === "true") {
+        return new Response("OK", { status: 200 });
+    }
 
-    if (response.ok) {
+    let backendRunning: boolean;
+    try {
+        const response = await fetch(env.get("MEDUSA_BACKEND_URL") + "/health");
+        backendRunning = response.ok;
+    } catch {
+        backendRunning = false;
+    }
+
+    if (backendRunning) {
         return new Response("OK", { status: 200 });
     } else {
         return new Response("DEAD", { status: 500 });
