50d29edcbbc1051b06f8dcf07980de220e429078
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2025-07-23 05:56:28 +0200

    refactor(slider): update size

--- src/lib/components/ui/slider/slider.svelte
+++ src/lib/components/ui/slider/slider.svelte
@@ -28,7 +28,7 @@ get along, so we shut typescript up by casting `value` to `never`.
     {#snippet children({ thumbs })}
         <span
             data-orientation={orientation}
-            class="relative grow overflow-hidden rounded-full bg-secondary data-[orientation='horizontal']:h-2 data-[orientation='vertical']:h-full data-[orientation='horizontal']:w-full data-[orientation='vertical']:w-2"
+            class="relative grow overflow-hidden rounded-full bg-secondary data-[orientation='horizontal']:h-1.5 data-[orientation='vertical']:h-full data-[orientation='horizontal']:w-full data-[orientation='vertical']:w-1.5 md:data-[orientation='horizontal']:h-1 md:data-[orientation='vertical']:w-1"
         >
             <SliderPrimitive.Range
                 class="absolute bg-primary data-[orientation='horizontal']:h-full data-[orientation='vertical']:w-full"
@@ -37,7 +37,7 @@ get along, so we shut typescript up by casting `value` to `never`.
         {#each thumbs as thumb (thumb)}
             <SliderPrimitive.Thumb
                 index={thumb}
-                class="block size-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+                class="block size-4 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 md:size-3"
             />
         {/each}
     {/snippet}
