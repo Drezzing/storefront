53c64478d0da15dba1989158d8c62771b0be279f
Lucas VALLEGEAS <lucas.vallegeas@etu.isima.fr>
2025-11-09 19:32:19 +0100

    remove testing page

--- src/routes/shipping/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    // Temp test page
-    import ParcelSelector from "$lib/components/ParcelSelector/ParcelSelector.svelte";
-    let selectedParcel = $state(null);
-    $inspect("parcelNum", selectedParcel);
-</script>
-
-<ParcelSelector bind:selectedParcel></ParcelSelector>
